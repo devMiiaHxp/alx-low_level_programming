79a6f37cf6560ee0e84f97f182f3744c608d64d1
devMiiaHxp <dopemiiahxy@gmail.com>
2024-02-12 16:02:45 -0800

    all files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	i = write(fd, text_content, len);
 
 	if (fd == -1 || i == -1)
